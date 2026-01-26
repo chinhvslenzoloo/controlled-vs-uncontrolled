59b33ac2604cd3de40065754e9c91652403109b9
chinhvslenzoloo <chinhvslenzoloo@gmail.com>
2026-01-26 10:38:46 +0800

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,47 @@
-// import { useState } from "react";
+import { useState } from "react";
+import { useWeather } from "./hooks/useWeather";
 
+function App() {
 
-// function ControlledForm(){
-//     // 1. input-iin utgiig hadgalah state
-//     const [name , setName]= useState("");
+  const [city, setCity] = useState("");
+  const [search, setSearch] = useState("");
 
+  const { data, loading, error } = useWeather(search);
 
-//     // 2. input oorchlogdoh vyd ajillah func
-//     const HandleChange = (e) =>{
-//         setName(e.target.value);
-//     };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearch(city);
+  };
 
+  return (
+    <div style={{ padding: "40px" }}>
 
-//     // 3.form submit hiih vyd
-//     const HandleSubmit = (e) =>{
-//         e.preventDefault(); //page refresh zogsoono
-//         alert(name);
-//     }
-//     return(
-//         <form onSubmit={HandleSubmit}>
-//             <input type="text" value={name} onChange={HandleChange} />
-//             <button type="submit">send</button>
-//         </form>
-//     )
-// };
+      <h1>🌤 Weather App</h1>
 
-// export default ControlledForm
+      <form onSubmit={handleSubmit}>
+        <input
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter city"
+        />
 
+        <button>Search</button>
+      </form>
 
-// import { useRef } from "react";
+      {loading && <p>Loading...</p>}
 
-// function UncontrolledForm(){
-//     // 1. inpout refrence vvsgej baigaa
-//     const inputRef = useRef(null);
-//     // 2. handler submit
-//     const Handlesubmit = (e) =>{
-//         e.preventDefault();
-//         // 3.Dom-oos valueg awn
-//         alert(inputRef.current.value);
-//     };
+      {error && <p>{error}</p>}
 
-//     return(
-//         <form onSubmit={Handlesubmit}>
-//             <input type="text" ref={inputRef}/>
-//             <button type="submits">send</button>
-//         </form>
-//     )
-// };
+      {data && (
+        <div>
+          <h2>{data.name}</h2>
+          <p>Temp: {data.main.temp} °C</p>
+          <p>Weather: {data.weather[0].main}</p>
+        </div>
+      )}
 
-// export default UncontrolledForm;
+    </div>
+  );
+}
 
-
-// MULTIPLE input example
-
-// import { useState } from "react";
-// // 1.react-iin state hadgalah hookiig oruulj irj baina
-//  function Form(){
-
-//     const [formData , setformData] = useState({
-//         email: "",
-//         passwornd: "",
-//     });
-//     // 2.formData state vvsgeh
-
-//     const HandleChange = (e) =>{
-//     // 3. Bvh inputd ashiglah neg l handler (e)=> event object
-//         const {name , value} = e.target;
-//     // 4.name value zadlah
-//         setformData({
-//             ...formData,
-//             [name]: value
-//         }); 
-//         // 5. ...formdata huuchin state-g huulj awch baina
-//         // 6.[name]: value dynamic key update hiij baina
-//     };
-//     return(
-//         <form>
-//             <input name="email" placeholder="Email" onChange={HandleChange} />
-//             <input name="password" placeholder="Password" type="password" onChange={HandleChange} />
-
-//         </form>
-//     )
-//  }
-
-//  export default Form;
+export default App;
